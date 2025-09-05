017b337f256226cf21a40360bd73fd95087b41c6
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-05 09:34:14 -0300

    feat: adicionar função draw_cat_plot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -16,19 +16,19 @@ df['gluc'] = (df['gluc'] > 1).astype(int)
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df,id_vars=["cardio"],value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
 
     # 7
-
+    catplot = sns.catplot(data=df_cat, x='variable', y='total', hue='value', col='cardio', kind='bar')
 
 
     # 8
-    fig = None
+    fig = catplot.fig
 
 
     # 9
