986cb8a9f5e596d4a4455a2f1a3bea92055d4dc6
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-05 12:14:53 -0300

    feat: adicionar função draw_heat_map e plotar os gráficos solicitados

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -39,21 +39,21 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 8))
 
     # 15
-
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", center=0, vmax=0.3, square=True, cbar_kws={"shrink": .5})
 
 
     # 16
